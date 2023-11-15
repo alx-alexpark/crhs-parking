070ae005f62e79dfb02658183db70fb92a781827
Alexander Park <me@parkalex.dev>
2023-11-15 17:02:55 -0600

    Finish /api/createDBUser

--- app/api/createDBUser/route.ts
+++ app/api/createDBUser/route.ts
@@ -1,11 +1,12 @@
+import dbConnect from "@/lib/dbConnect";
 import User from "@/models/User";
 import { currentUser } from "@clerk/nextjs";
 
-// TODO: change ths back to POST
-export async function GET(request: Request) {
+export async function POST(request: Request) {
+    await dbConnect();
     const user = await currentUser();
-    console.log("GET /api/createDBUser")
+    // console.log("GET /api/createDBUser")
     let name = user?.firstName + " " + user?.lastName;
     User.create({name: name, email: user?.emailAddresses[0].emailAddress});
-    return new Response("yes");
+    return new Response("User created");
 }
