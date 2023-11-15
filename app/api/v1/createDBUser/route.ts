5ed641f827e90d9264e7e2030867457cf3842aad
Alexander Park <me@parkalex.dev>
2023-11-15 17:11:54 -0600

    Prevent double user document creation on /api/v1/createDBUser

--- app/api/v1/createDBUser/route.ts
+++ app/api/v1/createDBUser/route.ts
@@ -5,8 +5,12 @@ import { currentUser } from "@clerk/nextjs";
 export async function POST(request: Request) {
     await dbConnect();
     const user = await currentUser();
-    // console.log("GET /api/createDBUser")
-    let name = user?.firstName + " " + user?.lastName;
-    User.create({name: name, email: user?.emailAddresses[0].emailAddress});
+    const name = user?.firstName + " " + user?.lastName;
+    const email = user?.emailAddresses[0].emailAddress;
+
+    if ((await User.find({ email: email })).length > 0) 
+        return new Response("User already exists");
+
+    User.create({name: name, email: email});
     return new Response("User created");
 }
