76571f268b80fc179f32358f440b9e3ce7c24685
supercolbat <supercolbat@protonmail.com>
2023-11-29 13:46:43 -0600

    fix(models/ParkingSpotRequest.ts): add missing String type to student name schema

--- app/api/v1/student/parkingSpotRequest/route.ts
+++ app/api/v1/student/parkingSpotRequest/route.ts
@@ -1,19 +1,45 @@
-import dbConnect from "@/lib/dbConnect";
-import ParkingSpotRequest from "@/models/ParkingSpotRequest";
-import User from "@/models/User";
-import { currentUser } from "@clerk/nextjs";
+import { currentUser } from '@clerk/nextjs';
+
+import dbConnect from '@/lib/dbConnect';
+import ParkingSpotRequest from '@/models/ParkingSpotRequest';
+import User from '@/models/User';
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  const user = await currentUser();
+  const dbUser = await User.findOne({
+    email: user?.emailAddresses[0].emailAddress,
+  });
+
+  const currentParkingSpotRequest = await ParkingSpotRequest.findOne({
+    user: dbUser._id,
+    submitted: false,
+  });
+
+  return new Response('hello ' + String(currentParkingSpotRequest));
+}
 
 // TODO: test this and make sure it works
 export async function PUT(request: Request) {
-    await dbConnect();
-    const newJson = request.json();
-    const user = await currentUser();
-    const dbUser = await User.findOne({email: user?.emailAddresses[0].emailAddress})
-    let currentParkingSpotRequest = await ParkingSpotRequest.findOne({user: dbUser._id, submitted: false});
-    if (currentParkingSpotRequest != undefined) {
-        await ParkingSpotRequest.updateOne({user : dbUser._id}, newJson);
-    } else {
-        await ParkingSpotRequest.create({user: dbUser._id, ...newJson});
-    }
-    return new Response("yes");
+  await dbConnect();
+
+  const newJson = request.json();
+  const user = await currentUser();
+  const dbUser = await User.findOne({
+    email: user?.emailAddresses[0].emailAddress,
+  });
+
+  const currentParkingSpotRequest = await ParkingSpotRequest.findOne({
+    user: dbUser._id,
+    submitted: false,
+  });
+
+  if (currentParkingSpotRequest != undefined) {
+    await ParkingSpotRequest.updateOne({ user: dbUser._id }, newJson);
+  } else {
+    await ParkingSpotRequest.create({ user: dbUser._id, ...newJson });
+  }
+
+  return new Response('yes');
 }
