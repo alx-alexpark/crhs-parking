f352c1ae3224d084360aacfca18cee8484de1a60
supercolbat <supercolbat@protonmail.com>
2024-03-05 13:59:50 -0600

    feat: Implement reviewer dialog and student todo list

--- app/dashboard/student-dashboard.tsx
+++ app/dashboard/student-dashboard.tsx
@@ -1,9 +1,13 @@
+'use client';
+
 import { currentUser, UserButton } from '@clerk/nextjs';
 import Link from 'next/link';
 
 import clsx from 'clsx';
 
+import { ParkingMap } from '@/components';
 import ParkingRequestHistory from './_components/student-list';
+import StudentTodoList from './_components/student-todo-list';
 import styles from './student-dashboard.module.scss';
 
 export default async function StudentDashboardPage() {
@@ -24,6 +28,11 @@ export default async function StudentDashboardPage() {
         </div>
       </nav>
 
+      {/* TODO: hide if all items are completed **on page load** */}
+      <section>
+        <StudentTodoList />
+      </section>
+
       {/* TODO: determine if this is a good style */}
       {/*       or wrap in div and add heropattern */}
       {user!.firstName ? (
@@ -43,16 +52,15 @@ export default async function StudentDashboardPage() {
           <h2>Your parking spot</h2>
           <p>You park in the [object Object] Quadrant.</p>
 
-          {/* <Map /> */}
-          <div
-            style={{
-              background: 'var(--color-primary)',
-              width: '100%',
-              height: '250px',
-              borderRadius: 'var(--border-container)',
-            }}
-          ></div>
-          {/* <Map /> */}
+          <ParkingMap />
+          {/* <div */}
+          {/*   style={{ */}
+          {/*     background: 'var(--color-primary)', */}
+          {/*     width: '100%', */}
+          {/*     height: '250px', */}
+          {/*     borderRadius: 'var(--border-container)', */}
+          {/*   }} */}
+          {/* ></div> */}
         </div>
         <div>
           <h2>Recent parking requests</h2>
