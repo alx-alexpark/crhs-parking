e7d948b3e42ebbf7e06063df7dc5df5e38a53a5e
Alexander Park <me@parkalex.dev>
2024-05-01 21:13:46 -0500

    Temporarily re-enable openstreetmap tile layer until we can replace with svg

--- components/ParkingMap/parking-map.tsx
+++ components/ParkingMap/parking-map.tsx
@@ -115,12 +115,12 @@ export function ParkingMap({
         content={isFullScreen ? exitFullScreenIcon : enterFullScreenIcon}
       />
 
-      {/* <TileLayer */}
-      {/*   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' */}
-      {/*   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" */}
-      {/*   maxNativeZoom={19} */}
-      {/*   maxZoom={22} */}
-      {/* /> */}
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' 
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+        maxNativeZoom={19} 
+        maxZoom={22} 
+      /> 
 
       <LayerGroup>
         <TextMarker
